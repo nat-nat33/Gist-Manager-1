2f09a590f9335a469deb9c4389fd49eb15eda52c
Taylor Kennedy <taykenned@gmail.com>
2016-05-11 18:41:25 -1000

    adds passport and gist route files for later

---
+++ passport/setupPassport.js
@@ -0,0 +1,33 @@
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
+
+function setUpPassport() {
+  passport.serializeuser(function(user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+  });
+
+  passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+}
+
+module.exports = setUpPassport();
