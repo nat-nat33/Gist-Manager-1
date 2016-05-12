cfa41744a1d93b349b30d1a895883b068ec4342b
Taylor Kennedy <taykenned@gmail.com>
2016-05-12 13:40:48 -1000

    adds webpack and scss configs

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: './public/js',
+    filename: 'browser-bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+    ]
+  }
+};
